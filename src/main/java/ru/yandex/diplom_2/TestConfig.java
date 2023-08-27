27fda5c59cf21f58cdf58f1ccb7f326e8764bdbb
Radchenko Aleksey <radchenko.aleksei@gmail.com>
2023-08-28 00:06:48 +0300

    problems with git

--- src/main/java/ru/yandex/diplom_2/TestConfig.java
+++ src/main/java/ru/yandex/diplom_2/TestConfig.java
@@ -2,6 +2,8 @@ package ru.yandex.diplom_2;
 
 public class TestConfig {
     public static final String BASE_URL = "https://stellarburgers.nomoreparties.site/";
+    public static final String BASE_URL_Login = "https://stellarburgers.nomoreparties.site/login";
+
 }
 
 // last
